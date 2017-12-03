e235b9d7dad3e1d487a114019faa234694dc8a8e
schreck <schreck@iastate.edu>
2017-12-03 14:46:25 -0600

    fuck

--- noise.js
+++ noise.js
@@ -1,38 +1,33 @@
 App = function()
 {
-    // this is where the WADE app is initialized
+	// this is where the WADE app is initialized
 	this.init = function()
 	{
-        // load a scene
+		// load a scene
 		wade.loadScene('scene1.wsc', true, function()
-        {
-            // the scene has been loaded, do something here
-        	var grass = 
+				{
+	var sand = 
+	{
+		baseColor: [202,177,50,255], 
+		noise: 
+		[
 			{
-				baseColor: [53,161,27,255], 
-				noise: 
-				[
-					{
-						color: [95, 235, 61, 180], 
-						attenuation: 2, 
-						roughness: 2,
-						numOctaves: 3,
-						startingOctave: 2
-					},
-					{
-						color: [200, -60, 0, 2550], 
-						attenuation: 2, 
-						roughness: 2,
-						numOctaves: 5,
-						startingOctave: 2
-					}
-				]
-			};
-        	wade.setImage('grass', generateTexture(256, grass));
-        	var obj = new SceneObject(new Sprite('grass'));
-        	wade.addSceneObject(obj);
-            
-        });
+				color: [235, 221, 61, 1024], 
+				attenuation: 1.5, 
+				roughness: 2,
+				numOctaves: 4,
+				startingOctave: 2
+			},
+			{
+				color: [255, 255, 255, 255], 
+				attenuation: 1.5, 
+				roughness: 6,
+				numOctaves: 3,
+				startingOctave: 5
+			}			
+		]
+	};
+				});
 	};
 };
 
