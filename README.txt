150c9635d5c821f2c0d439b30e3f88e9040126c9
schreck <schreck@iastate.edu>
2017-12-04 03:46:12 -0600

    Added my final generated textures, notice that most of the attenuation and roughness were done by hours of trial and error

--- README.txt
+++ README.txt
@@ -1,1 +1,20 @@
 Flame generator using perlin noise
+
+The perlin.js is from a tutorial online, that is where I got most of the calculations
+
+
+
+You must first run the command 'python -m SimpleHTTPServer XXXX' to spin up a local server and get access to the wade game engine.
+Once that has ran, noise.js contains the shader data necessary for the construction of the alpha, red, and green channels -- we can experiment on those later.
+
+Note: I used the b.png as an alpha mask as described in the tutorial however and used that image they had.
+
+
+Once we have all of the four channels make sure you have imagemagick installed.
+Note: if the texture was generated with color imagemagick can grayscale via 'convert name.png -channel RGBA -matte -colorspace gray name.png'
+
+To combine the four channels of the texture we can utilize the following command:
+	'convert r.png g.png b.png a.png -channel RGBA -combine test_flame.png'
+
+
+
--- noise.js
+++ noise.js
@@ -7,23 +7,66 @@ App = function()
  		wade.loadScene('scene1.wsc', true, function()
         {
              // the scene has been loaded, do something here
+/*
+			//This is the alpha channel
 			var test =
 			{
-				baseColor: [202,177,50,255],
+				baseColor:[0,0,0,100],
 				noise:
-					[
-						{
-							color: [235, 221, 61, 1024],
-							attenuation: 1.5,
-							roughness: 2,
-							num_octs: 4,
-							starting_oct: 2
-						}
-					]
+				[
+					{
+						color:[255,255,255,3750],
+						//this makes me darker
+						attenuation: 1.2,
+						roughness: 1.7,
+						num_octs: 8,
+						starting_oct: 2
+					}
+				]
 			};
 			wade.setImage('test',texture_factory(256,test));
 			var obj = new SceneObject(new Sprite('test'))
 			wade.addSceneObject(obj);
+*/
+/*
+			var g_channel = 
+			{
+				baseColor:[0,0,0,100],
+				noise:
+				[
+					{
+						color: [255,255,255,2550],
+						attenuation: 1.3,
+						roughness: 1.8,
+						num_octs: 5,
+						starting_oct: 2
+					}
+				]
+			};
+			wade.setImage('g_channel',texture_factory(256,g_channel));
+			var obj = new SceneObject(new Sprite('g_channel'));
+			wade.addSceneObject(obj);
+*/
+/*
+			var r_channel = 
+			{
+//				baseColor:[255,255,255,255],
+				baseColor: [0,0,0,100],
+				noise:
+				[
+					{
+						color: [255,255,255,4096],
+						attenuation: 1.2,
+						roughness: 1.4,
+						num_octs:6,
+						starting_oct: 2
+					}
+				]
+			};
+			wade.setImage('r_channel',texture_factory(256,r_channel));
+			var obj = new SceneObject(new Sprite('r_channel'));
+			wade.addSceneObject(obj);
+*/
          });
  	};
  };
